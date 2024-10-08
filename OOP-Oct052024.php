9b26cf596aaee1e28ff24ec2a4819d43d6078c79
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2024-10-08 13:08:59 +0500

    Constructor/Destructor

--- OOP-Oct052024.php
+++ OOP-Oct052024.php
@@ -1,23 +1,53 @@
 <?php
+    class Teacher {
+        public $name;
+
+        function __construct($fullname)
+        {
+            $this->name = $fullname;
+        }
+        function __destruct() {
+            echo "<p>The Teacher {$this->name} object is destructed or the script is stopped or exited</p>";
+        }
+
+    }
     class Student {
+        // Properties
+        private $id;
         public $name;
         public $age;
         public $email;
         public $gender;
+
+        // Constructor
+        function __construct($gr_number,$fullname) {
+            $this->id = $gr_number;
+            $this->name = $fullname;
+        }
         
-        public function show_bio () {
+        // Methods
+        function show_bio () {
             echo "<ul>";
+                echo "<li><b>GR # :</b>" . $this->id ."</li>";
                 echo "<li><b>Name :</b>" . $this->name ."</li>";
                 echo "<li><b>Age :</b>" . $this->age ."</li>";
                 echo "<li><b>Email :</b>" . $this->email ."</li>";
                 echo "<li><b>Gender :</b>" . $this->gender ."</li>";
             echo "</ul>";
         }
+        // Destructor
+        function __destruct() {
+            echo "<p>The Student {$this->name} object is destructed or the script is stopped or exited</p>";
+        }
     }
     
-    $s1 = new Student();
-    $s2 = new Student();
+    $s1 = new Student(1,"Rizwan");
+    $s2 = new Student(2,"Muniba");
 
+    $t1 = new Teacher("Murtaza");
+
+    $s1->show_bio();
+    $s2->show_bio();
 
     $s1->name = "Rizwan Naqvi";
     $s1->age = 42;
@@ -31,3 +61,8 @@
 
     $s1->show_bio();
     $s2->show_bio();
+
+
+    var_dump($s1 instanceof Student);
+    echo "<br>";
+    var_dump($s2 instanceof Teacher);
